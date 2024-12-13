d169086e26f050e3d786c1bf9c57fe4978feff07
Eric Scott <scottericr@gmail.com>
2024-12-13 16:47:06 -0700

    add docs and aspirational arguments

--- R/download_datamart_zip.R
+++ R/download_datamart_zip.R
@@ -1,4 +1,15 @@
-download_datamart_zip <- function(states, rawdat_dir = "data/rawdat/state/") {
+#' Download zip files from FIA datamart
+#' 
+#' The zip files are smaller than just the *_TREE.csv, so this just downloads the whole zip and extracts the required CSV files (TREE)
+#' uses curl::multi_download which resumes and skips partial and incomplete downloads, respectively, when run subsequent times
+#' @param states vector of state abbreviations
+#' @param rawdat_dir where to save the zip files
+#' @param extract logical; extract the TREE and PLOT csv files?
+#' @param keep_zip logical; keep the .zip file after CSVs are extracted?
+download_zip_from_datamart <- function(states,
+                                       rawdat_dir = "data/rawdat/state/",
+                                       extract = TRUE,
+                                       keep_zip = FALSE) {
   states <- match.arg(states, state.abb, several.ok = TRUE)
   base_url <- "https://apps.fs.usda.gov/fia/datamart/CSV/"
   files <- paste0(states, "_CSV.zip")
@@ -16,6 +27,18 @@ download_datamart_zip <- function(states, rawdat_dir = "data/rawdat/state/") {
   
   #TODO: check response for issues, retries, whatever
   
-  #return
   resp$destfile
+  
+  #TODO extract and remove zip if desired
+  #might want to do this one state at a time??
+  if (isTRUE(extract)) {
+    cli::cli_inform("Extracting CSVs from .zip files")
+    #pull out the CSVs of interest for each state
+  }
+  
+  if (isTRUE(extract) & isFALSE(keep_zip)) {
+    cil::cli_inform("Removing .zip files")
+    fs::file_delete(resp$destfile)
+  }
+  
 }
