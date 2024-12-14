a02a409d1fd14704fdc1c5a1008c46d7cec3885b
Eric Scott <scottericr@gmail.com>
2024-12-13 17:02:42 -0700

    hook up args to extract and delete zip

--- R/download_datamart_zip.R
+++ R/download_datamart_zip.R
@@ -27,18 +27,22 @@ download_zip_from_datamart <- function(states,
   
   #TODO: check response for issues, retries, whatever
   
-  resp$destfile
-  
-  #TODO extract and remove zip if desired
-  #might want to do this one state at a time??
-  if (isTRUE(extract)) {
-    cli::cli_inform("Extracting CSVs from .zip files")
+  zips <- resp$destfile
+  if (isFALSE(extract)) {
+    return(zips)
+  } else {
+    cli::cli_alert_info("Extracting CSVs from .zip files")
     #pull out the CSVs of interest for each state
+    lapply(zips, \(zip) {
+      csvs <- c(
+        gsub("CSV.zip", "TREE.csv", fs::path_file(zip)),
+        gsub("CSV.zip", "PLOT.csv", fs::path_file(zip))
+      )
+      unzip(zip, files = csvs, exdir = rawdat_dir)
+      if (isFALSE(keep_zip)) {
+        cli::cli_alert_info("Removing .zip file")
+        fs::file_delete(zip)
+      }
+    })
   }
-  
-  if (isTRUE(extract) & isFALSE(keep_zip)) {
-    cil::cli_inform("Removing .zip files")
-    fs::file_delete(resp$destfile)
-  }
-  
 }
