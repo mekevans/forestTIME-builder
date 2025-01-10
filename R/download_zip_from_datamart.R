d044c3cf197ad34b35d7733a37e4ef9be8fd255d
Eric Scott <scottericr@gmail.com>
2025-01-10 10:44:44 -0700

    if zip already downloaded, but no CSVs, just extract them

--- R/download_zip_from_datamart.R
+++ R/download_zip_from_datamart.R
@@ -18,15 +18,33 @@ download_zip_from_datamart <- function(states,
   out_paths <- fs::path(rawdat_dir, files)
   urls <- URLencode(paste0(base_url, files))
   
+  #check if .zip is already downloaded
+  zip_check <- purrr::map_chr(states, \(state) {
+    fs::path(rawdat_dir, paste0(state, "_CSV.zip"))
+  }) |> fs::file_exists()
+  
   #check if .csvs are there from a previous run with keep_zip = FALSE
   csv_check <- 
     purrr::map(states, \(state) {
       fs::path(rawdat_dir, paste0(state, c("_TREE.csv", "_PLOT.csv", "_COND.csv")))
     }) |> 
     purrr::map_lgl(\(x) all(fs::file_exists(x)))
-  states <- states[!csv_check]
   
-  if(length(states) == 0) {
+  #if zip is there, but not CSVs just unzip
+  if (any(zip_check & !csv_check) & isTRUE(extract)) {
+    unzip_csvs(names(zip_check)[zip_check & !csv_check], keep_zip = keep_zip)
+    #update CSV check
+    csv_check <- 
+      purrr::map(states, \(state) {
+        fs::path(rawdat_dir, paste0(state, c("_TREE.csv", "_PLOT.csv", "_COND.csv")))
+      }) |> 
+      purrr::map_lgl(\(x) all(fs::file_exists(x)))
+  }
+  #only download what is needed
+  urls <- urls[!csv_check]
+  out_paths <- out_paths[!csv_check]
+  
+  if(length(urls) == 0) {
     cli::cli_warn("All CSVs already downloaded!")
     return(NULL)
   }
@@ -46,19 +64,25 @@ download_zip_from_datamart <- function(states,
   if (isFALSE(extract)) {
     return(zips)
   } else {
-    cli::cli_alert_info("Extracting CSVs from .zip files")
-    #pull out the CSVs of interest for each state
-    lapply(zips, \(zip) {
-      csvs <- c(
-        gsub("CSV.zip", "TREE.csv", fs::path_file(zip)),
-        gsub("CSV.zip", "PLOT.csv", fs::path_file(zip)),
-        gsub("CSV.zip", "COND.csv", fs::path_file(zip))
-      )
-      unzip(zip, files = csvs, exdir = rawdat_dir)
-      if (isFALSE(keep_zip)) {
-        cli::cli_alert_info("Removing .zip file")
-        fs::file_delete(zip)
-      }
-    })
+    unzip_csvs(zips, keep_zip)
+    return(NULL)
   }
+}
+
+unzip_csvs <- function(zips, keep_zip) {
+  cli::cli_alert_info("Extracting CSVs from .zip files")
+  #pull out the CSVs of interest for each state
+  lapply(zips, \(zip) {
+    csvs <- c(
+      gsub("CSV.zip", "TREE.csv", fs::path_file(zip)),
+      gsub("CSV.zip", "PLOT.csv", fs::path_file(zip)),
+      gsub("CSV.zip", "COND.csv", fs::path_file(zip))
+    )
+    unzip(zip, files = csvs, exdir = rawdat_dir)
+    if (isFALSE(keep_zip)) {
+      cli::cli_alert_info("Removing .zip file")
+      fs::file_delete(zip)
+    }
+  })
+  return(invisible(TRUE))
 }
