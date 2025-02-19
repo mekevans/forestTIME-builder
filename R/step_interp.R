61cb0d2a1f216de4cbb106f53ebdaaf5e0055d91
Eric Scott <scottericr@gmail.com>
2025-02-19 11:41:22 -0700

    add step_interp() function

---
+++ R/step_interp.R
@@ -0,0 +1,46 @@
+#' Interpolate categorical variables to switch at midpoint
+#' 
+#' Categorical variables like `DECAYCD` can't be linearly interpolated between
+#' inventory years.  Instead, we assume they switch values at the midpoint
+#' (rounded up) between non-missing values.  Trailing NAs are replaced with the
+#' last non-na value and leading NAs are returned as-is.
+#'
+#' @param x a vector
+#'
+#' @returns a vector with no NAs
+#' @export
+#'
+#' @examples
+#' step_interp(c(NA, NA, "A", NA, NA, NA, "B", NA, NA, NA, NA, "C", NA))
+step_interp <- function(x) {
+  if (all(is.na(x))) {
+    return(x)
+  }
+  x_non_nas <- x[!is.na(x)]
+  i <- seq_along(x)
+  
+  # index at which each non-missing value is
+  i_non_nas <- i[!is.na(x)]
+  
+  # count leading NAs
+  leading_NAs <- i_non_nas[1] - 1
+  
+  #this is the number of rep()s on the right side (not including the non-missing value)
+  right <- floor((dplyr::lead(i_non_nas) - i_non_nas) / 2)
+  right[is.na(right)] <- 0
+  
+  #number of rep()s to the left, including the non-missing value
+  left <- ceiling((i_non_nas - dplyr::lag(i_non_nas)) / 2)
+  left[is.na(left)] <- 1
+  
+  times <- left + right
+  
+  #extrapolate trailing NAs
+  #reps of last value gets adjusted so sum(times) adds up to the total length of the input
+  times[length(times)] <- length(x) - (sum(times) - times[length(times)] + leading_NAs)
+  
+  x_interp <- purrr::map2(x_non_nas, times, rep) |> purrr::list_c()
+  
+  #add back leading NAs
+  c(rep(NA, leading_NAs), x_interp)
+}
