72a538ae21c3ab2c1d834180f2561deb25e6e888
Eric Scott <scottericr@gmail.com>
2025-04-03 08:42:11 -0700

    document adjust_mortality()

--- R/adjust_mortality.R
+++ R/adjust_mortality.R
@@ -1,4 +1,22 @@
-source("R/utils.R")
+source("R/utils.R") #for `%|||%` operator
+
+#' Adjust interpolated tables for mortality
+#'
+#' Trees in the input `data_interpolated` already have had their switch to
+#' `STATUSCD` 2 (i.e. death) interpolated to the midpoint (rounded down) between
+#' it's last survey alive and first survey dead.
+#'
+#' This does the following:
+#' - Optionally figures out if a tree has a recorded `MORTYR` and uses that for
+#'   the transition to `STATUSCD` 2 instead of the interpolated values. (assumes
+#'   `MORTYR` was *before* the inventory where it was recoreed dead)
+#' - Drops trees that transition to `STATUSCD` 0 and `RECONCILECD` 5, 6, or 9
+#'   (moved out of plot) at the midpoint between surveys
+#' - Adjusts `STANDING_DEAD_CD` so that it only applies to dead trees
+#' - Adjusts `DECAYCD` so that it only applies to standing dead trees
+#' - Adjusts `DIA`, `HT`, `ACTUALHT`, `CULL`, and `CR` so that they only apply
+#'   to live or standing dead trees.
+#' - Removes the `MORTYR` column, as it is no longer needed
 adjust_mortality <- function(data_interpolated, use_mortyr = TRUE) {
   cli_progress_step("Adjusting for mortality")
 
@@ -10,6 +28,7 @@ adjust_mortality <- function(data_interpolated, use_mortyr = TRUE) {
         first_dead = ifelse(
           test = any(!is.na(MORTYR)), #has recorded MORTYR
           yes = max(MORTYR, na.rm = TRUE),
+          # see utils.R for more info on what %|||% does
           no = YEAR[min(which(STATUSCD == 2) %|||% NA)]
         )
       )
@@ -17,6 +36,7 @@ adjust_mortality <- function(data_interpolated, use_mortyr = TRUE) {
     df <- data_interpolated |>
       group_by(tree_ID) |>
       mutate(
+        # see utils.R for more info on what %|||% does
         first_dead = YEAR[min(which(STATUSCD == 2) %|||% NA)]
       )
   }
