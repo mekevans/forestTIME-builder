6d30bf93be7014b92a30fd7698aba3eafd5aad59
Eric Scott <scottericr@gmail.com>
2025-05-15 09:58:09 -0700

    fix check() warning

--- tests/testthat/test-expand_data.R
+++ tests/testthat/test-expand_data.R
@@ -1,14 +1,13 @@
-# fmt: skip
-df <- tibble::tribble( 
-  ~plot_ID, ~tree_ID, ~INVYR, ~HT, ~DIA, ~SPCD,
-  "p1", "A", 2000, 12, 24, 123,
-  "p1", "A", 2005, 18, 26, 123,
-  "p1", "B", 2000, 5, 10, 222,
-  "p1", "B", 2005, 7, 11, 222
-)
-
-
 test_that("expand_data() works", {
+  # fmt: skip
+  df <- dplyr::tribble( 
+    ~plot_ID, ~tree_ID, ~INVYR, ~HT, ~DIA, ~SPCD,
+    "p1", "A", 2000, 12, 24, 123,
+    "p1", "A", 2005, 18, 26, 123,
+    "p1", "B", 2000, 5, 10, 222,
+    "p1", "B", 2005, 7, 11, 222
+  )
+
   df_expanded <- expand_data(df)
   expect_snapshot(df_expanded) #TODO comment out if this gets annoying
   expect_gt(nrow(df_expanded), nrow(df))
