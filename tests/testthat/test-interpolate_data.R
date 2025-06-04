320d1b888e3e6310ee8c399240eb6797b2ee3e3a
Eric Scott <scottericr@gmail.com>
2025-06-04 14:59:45 -0700

    add tests

--- tests/testthat/test-interpolate_data.R
+++ tests/testthat/test-interpolate_data.R
@@ -19,12 +19,49 @@ test_that("variables with NAs get interpolated correctly", {
       DECAYCD,
       DESIGNCD
     )
-  data_interpolated <- data |> 
-    expand_data() |> 
-    interpolate_data() 
+  data_interpolated <- data |>
+    expand_data() |>
+    interpolate_data()
 
   expect_equal(
     data_interpolated |> filter(YEAR %in% 2017:2020) |> pull(STANDING_DEAD_CD),
     rep(0, 4)
+  )
+})
+
+test_that("interpolation flags negative numbers as fallen dead", {
+  db <- read_fia(
+    "DE",
+    dir = system.file("exdata", package = "forestTIME.builder")
+  )
+  data <- prep_data(db) |>
+    dplyr::filter(tree_ID %in% c("10_1_1_104_3_4", "10_1_1_148_4_2")) |>
+    dplyr::select(
+      plot_ID,
+      tree_ID,
+      SPCD,
+      INVYR,
+      DIA,
+      HT,
+      ACTUALHT,
+      STATUSCD,
+      STANDING_DEAD_CD,
+      DECAYCD,
+      DESIGNCD
+    )
+  data_interpolated <- data |>
+    expand_data() |>
+    interpolate_data()
+
+  expect_equal(
+    data_interpolated |>
+      filter(ACTUALHT < 4.5) |>
+      pull(STANDING_DEAD_CD) |>
+      unique(),
+    0
+  )
+  expect_equal(
+    data_interpolated |> filter(ACTUALHT < 4.5) |> pull(STATUSCD) |> unique(),
+    2
   )
 })
